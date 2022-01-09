7934794db99379d9aba31452aa799f09091fa388
James9234 <rt5604@gmail.com>
2022-01-09 11:41:13 +0900

    Add for

--- udp_client.py
+++ udp_client.py
@@ -3,6 +3,7 @@ import socket
 import sys
 from util import *
 import sysutil
+import time
 
 logger = logging.getLogger()
 
@@ -26,12 +27,15 @@ def main(host='127.0.0.1', port=9999):
         print('client received: {} {}'.format(addr, data))
         addr = msg_to_addr(data)
 
-        ipAddr = sysutil.get_ip()
-        ipAddrEnc = ipAddr.encode()
-        print('client: send {} to {}'.format(ipAddr, addr))
-        sock.sendto(ipAddrEnc, addr)
-        data, addr = sock.recvfrom(1024)
-        print('client received: {} {}'.format(addr, data))
+        for i in range(5):
+            data = ipAddr + ':' + str(i)
+            data = data.encode()
+            print('client: send {} to {}'.format(data, addr))
+            sock.sendto(data, addr)
+            data, addr = sock.recvfrom(1024)
+            data = data.decode()
+            print('client received: {} {}'.format(addr, data))
+            time.sleep(1)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
